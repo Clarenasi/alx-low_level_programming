40d967fdf5766fa60ceb5ec263d69085631175dd
Clarenasi <claremosoba@gmail.com>
2022-06-30 12:04:57 -0700

    My second commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,8 +8,6 @@
  */
 int main(void)
 {
-	char ptr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	size_t fwrite(const void ptr, size_t size, FILE * restrict ,stream);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
