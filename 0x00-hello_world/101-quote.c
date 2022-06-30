7e10d1387e729f9e43363407c6abb58bf723f554
Clarenasi <claremosoba@gmail.com>
2022-06-30 10:23:08 -0700

    My second commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char ptr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	size_t fwrite(const void ptr, size_t size, FILE * restrict ,stream);
+	return (1);
+}
