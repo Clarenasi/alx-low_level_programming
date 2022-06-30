dd4188d8464a9ed15a22de67fd6c382f8b6badb2
Clarenasi <claremosoba@gmail.com>
2022-06-30 07:29:27 -0700

    Mysecond commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Succes)
  */
-int main(void) 
+int main(void)
 {
-	char *str = "\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }
