9cc003ce8cc0b1f3bf5d53c2939f4f9dea6a8eee
Clarenasi <claremosoba@gmail.com>
2022-06-30 06:58:57 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -c 4-main.c -o 4-main.o
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void) 
+{
+	char *str = "\"Programming is like building a multilingual puzzle\n";
+	puts(str);
+	return (0);
+}
