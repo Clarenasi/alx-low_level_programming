0315f681fed828d1d87d3b0e1a1190c75c63ee64
Clarenasi <claremosoba@gmail.com>
2022-07-05 14:11:20 -0700

    My first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  * print_alphabet_x10 - Prints 10 times the alphabet,
  * in lowercase, followed by a new line.
  *
- * Return: Always x10 a-z.
+ * Return: x10 a-z.
  */
 void print_alphabet_x10(void)
 {
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - checks for lowercase character.
+ * @c: single letter input
  *
  * Return: 1 if c is lowercase, 0 if otherwise
  */
