86aea1ca23b1f2dd900a4411cd649fdf611d9a14
Clarenasi <claremosoba@gmail.com>
2022-07-07 16:18:42 -0700

    Last commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#MORE FUNCTIONS AND NESTED LOOPS
+This projecthas more on functions and nested loops in C.
