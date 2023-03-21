4fb4f3bd12015728d106a366c0fb637a1edb5ff4
Clarenasi <claremosoba@gmail.com>
2023-03-21 02:37:51 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an int as a parameter
+ * updates the value it points to to 98
+ * @n: address to array n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
